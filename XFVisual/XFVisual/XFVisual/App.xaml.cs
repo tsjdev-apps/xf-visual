e0eb65ff4b623ca4909e38a05984c1165e89985a
Sebastian Jensen <apps@tsjdev-apps.de>
2019-10-01 11:43:54 +0200

    - change start page to mainpage

--- XFVisual/XFVisual/XFVisual/App.xaml.cs
+++ XFVisual/XFVisual/XFVisual/App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using XFVisual.Views;
 
 namespace XFVisual
 {
@@ -10,7 +9,7 @@ namespace XFVisual
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
