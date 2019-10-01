f982d92214bf06c50bba402254a1cd747befee10
Sebastian Jensen <apps@tsjdev-apps.de>
2019-10-01 11:43:03 +0200

    - update mainpage

---
+++ XFVisual/XFVisual/XFVisual/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFVisual.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public ICommand NavigateCommand { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(
+                async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await Navigation.PushAsync(page);
+                });
+            BindingContext = this;
+        }
+    }
+}
