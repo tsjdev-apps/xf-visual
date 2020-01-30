13a29e9afcf6a4718ae739c9a8da42255df6fbb2
Sebastian Jensen <apps@tsjdev-apps.de>
2020-01-30 08:27:21 +0100

    - update editorpage to use tabbedpage

--- XFVisual/XFVisual/XFVisual/Views/EditorPage.xaml.cs
+++ XFVisual/XFVisual/XFVisual/Views/EditorPage.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XFVisual.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class EditorPage : ContentPage
+    public partial class EditorPage : TabbedPage
     {
         public EditorPage()
         {
